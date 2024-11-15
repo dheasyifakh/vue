45b5e5245c893566f1178861e8f7729016b7c040
Dhea Syifa Khoirunnisa <dheasyifa.khoirunnisa@spentera.com>
2024-11-15 18:57:51 +0700

    First commit & learn if else in vue

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+  export default{
+    data(){
+      return{
+        name: 'John Doe',
+        status: false
+      }
+    }
+  }
+</script>
+
+<template>
+  <h1>{{ name }}</h1>
+  <!-- Learn if else in Vue -->
+  <p v-if="status">User is active</p>
+  <p v-else="status">User is inactive</p>
+</template>
+
